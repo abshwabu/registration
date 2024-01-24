908cf06f05cd8671c0085232afe56d7ba920d5ea
ashwabu <abdulazizredi90@gmail.com>
2024-01-24 12:17:04 +0300

    ignore intervention

--- lib/constants/apikey.dart
+++ lib/constants/apikey.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: constant_identifier_names
+
 const String create_apikey = 'http://10.0.2.2:8000/api/user/token/';
 const String register_apikey = 'http://10.0.2.2:8000/api/user/create/';
 const String get_apikey = 'http://10.0.2.2:8000/api/user/me/';
