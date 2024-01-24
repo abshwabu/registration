d6b1353106133a5f0ca24b0b1065d0ffbda79d1e
ashwabu <abdulazizredi90@gmail.com>
2024-01-24 12:18:00 +0300

    ignore

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: library_private_types_in_public_api, prefer_const_constructors, avoid_print, use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:gap/gap.dart';
