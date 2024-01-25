86bf98102ce8fe38fad012830a10353bf417315d
ashwabu <abdulazizredi90@gmail.com>
2024-01-25 09:12:56 +0300

    main page login

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:registration/pages/login.dart';
 import 'package:registration/pages/registration.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:RegistrationPage(),
+      home:LoginPage(),
     );
   }
 }
