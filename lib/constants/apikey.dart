a75ebf0a2b0d310c42259266f279145bd25da8eb
ashwabu <abdulazizredi90@gmail.com>
2024-02-09 18:03:05 +0300

    posts api

--- lib/constants/apikey.dart
+++ lib/constants/apikey.dart
@@ -3,3 +3,4 @@
 const String create_apikey = 'http://10.0.2.2:8000/api/user/token/';
 const String register_apikey = 'http://10.0.2.2:8000/api/user/create/';
 const String get_apikey = 'http://10.0.2.2:8000/api/user/me/';
+const String Post = 'http://10.0.2.2:8000/api/posts/posts/';
