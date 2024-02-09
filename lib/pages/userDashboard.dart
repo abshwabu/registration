779ac99e13402ad449c0a1077f9b3b5f42927f02
ashwabu <abdulazizredi90@gmail.com>
2024-02-09 21:49:47 +0300

    get posts

--- lib/pages/userDashboard.dart
+++ lib/pages/userDashboard.dart
@@ -1,6 +1,10 @@
-import 'package:flutter/material.dart';
-import 'package:registration/pages/UpdatePage.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:registration/constants/apikey.dart';
+import 'package:registration/pages/UpdatePage.dart';
+import 'package:http/http.dart' as http;
 class UserDashboard extends StatelessWidget {
   final String username;
   final String email;
@@ -37,9 +41,44 @@ class UserDashboard extends StatelessWidget {
             const SizedBox(width: 10,),
             Text('Email: $email'),
             ],
-          )
+          ),
+          PostList(),
         ]),
       ),
     );
+  }
+}
+
+class PostList extends StatefulWidget {
+
+  const PostList({super.key});
+
+  @override
+  State<PostList> createState() => _PostListState();
+}
+
+class _PostListState extends State<PostList> {
+  final storage = FlutterSecureStorage();
+
+  void get_posts() async {
+    String? token = await storage.read(key: 'authToken');
+
+    if (token == null){
+      print('Token not found');
+    }
+    http.Response response = await http.get(
+      Uri.parse(Post_apikey),
+      headers:  <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Authorization': 'Token $token',
+        },
+    );
+    String? title = response.body;
+    String data = jsonDecode(title);
+    print(data);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
