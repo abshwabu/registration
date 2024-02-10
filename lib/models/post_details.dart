80dc4a044cffe8295a9f952f8999f35c60d76d10
ashwabu <abdulazizredi90@gmail.com>
2024-02-10 06:26:35 +0300

    model for post details

---
+++ lib/models/post_details.dart
@@ -0,0 +1,8 @@
+class PostDetails{
+  int id;
+  String title;
+  String content;
+  String image;
+  List<Object> tags;
+  PostDetails({required this.id, required this.title, required this.content, required this.image, required this.tags});
+}
