1b2aa175bc544589223af439db3074ca808085a3
ashwabu <abdulazizredi90@gmail.com>
2024-02-10 06:26:12 +0300

    edited

--- lib/models/posts.dart
+++ lib/models/posts.dart
@@ -1,8 +1,6 @@
 class Post{
   int id;
   String title;
-  String content;
-  String image;
   List<Object> tags;
-  Post({required this.id, required this.title, required this.content, required this.image, required this.tags});
+  Post({required this.id, required this.title, required this.tags});
 }
