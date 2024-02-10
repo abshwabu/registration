606ff83cc9eaaa9f87ddc35542e1d3bfbb02d270
ashwabu <abdulazizredi90@gmail.com>
2024-02-10 05:52:58 +0300

    model for posts

---
+++ lib/models/posts.dart
@@ -0,0 +1,8 @@
+class Post{
+  int id;
+  String title;
+  String content;
+  String image;
+  List<Object> tags;
+  Post({required this.id, required this.title, required this.content, required this.image, required this.tags});
+}
