46de1097f5cf64bbfd4d0278bac2e30b49041548
ashwabu <abdulazizredi90@gmail.com>
2024-02-14 16:03:22 +0300

    add page

--- lib/pages/postDetails.dart
+++ lib/pages/postDetails.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:registration/pages/addProduct.dart';
 import 'package:registration/widgets/post_details_container.dart';
 
 class PostDetailsPage extends StatelessWidget {
@@ -10,28 +9,14 @@ class PostDetailsPage extends StatelessWidget {
   final List tags;
   const PostDetailsPage({super.key, required this.id, required this.title, required this.content, required this.image, required this.tags});
 
-  void navigateToAddPostPage(BuildContext context) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => AddPostPage()),
-    );
-  }
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        PostDetailsContainer(
-          id: this.id,
-          title: this.title,
-          content: this.content,
-          image: this.image,
-          tags: this.tags
-        ),
-        ElevatedButton(
-              onPressed: ()=> navigateToAddPostPage(context),
-              child:const Text('Add Post'),
-            )
-      ],
+    return PostDetailsContainer(
+      id: this.id,
+      title: this.title,
+      content: this.content,
+      image: this.image,
+      tags: this.tags
     );
   }
 }
